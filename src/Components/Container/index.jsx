39c25a550960ac95e49d1326c6be4da8fe94217d
Juan Morales <juan1810.jm@gmail.com>
2023-05-02 21:48:33 -0300

    2/5/23 style components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-
+import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
+import List from "../List";
+
+const ContainerStyled = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+const StylesContent = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`
 
 const Container = () => {
   return (
-    <div className="container">
+    <ContainerStyled>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StylesContent>
         <Account />
-      </section>
-    </div>
+        <List />
+      </StylesContent>
+    </ContainerStyled>
   );
 };
 
